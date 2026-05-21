260481cf67e301c8039a50e00466d81278cc9f42
Hhaza2 <145927357+Hhaza2@users.noreply.github.com>
2026-05-20 22:32:31 -0600

    Bug de seeders y corrección de tabla lotes
    Se arreglo el bug que no permitia ejecutar los seeders ya que estaba mal ordenados y ademas se corrigio la tabla lotes quitando el campo de codigo_producto que no se hizo uso.

--- database/migrations/2026_04_27_022717_create_lotes_table.php
+++ database/migrations/2026_04_27_022717_create_lotes_table.php
@@ -18,14 +18,12 @@ return new class extends Migration
             // dejé ubicacion_id comentada por si la ocupas más adelante. 
             // puedes descomentarla si la necesitas. 
             // este campo puede ser útil para saber dónde se encuentra físicamente cada lote dentro del almacén o bodega.
-            // $table->foreignId('ubicacion_id')->nullable()->constrained('ubicaciones');
-
+            $table->foreignId('ubicacion_id')->nullable()->constrained('ubicaciones');
             $table->string('codigo_lote')->unique(); 
-            $table->string('codigo_producto');
             $table->integer('cantidad_inicial');
             $table->integer('cantidad_actual');
             $table->date('fecha_entrada');
-            $table->date('fecha_vencimiento')->nullable(); // nullable() por si algun insumo no caduca
+            $table->date('fecha_vencimiento')->nullable();
             $table->string('proveedor'); 
             $table->enum('estado', ['activo', 'agotado', 'vencido'])->default('activo');
             $table->foreignId('registrado_por')->constrained('users');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //RoleSeeder::class,
-            //UserSeeder::class,
             EstadoSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
             CategoriaSeeder::class,
             UbicacionSeeder::class,
             AreaSeeder::class,
